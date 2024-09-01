204b6a642c789d29dc649b753c40799c76ab80b3
Kusal Debnath <debntahk1997@gmail.com>
2024-08-31 23:53:31 -0400

    Add bash

---
+++ bash/run_gvae_dense_rnn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ensure the script takes in a mode argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 {train|test}"
+    exit 1
+fi
+
+PROT_ENCODING='rnn'
+
+MODE=$1
+OUTPUT_LOG="../result_logs/output_gvae_dense_${PROT_ENCODING}_${MODE}.log"
+ERROR_LOG="../result_logs/error_gvae_dense_${PROT_ENCODING}_${MODE}.log"
+
+# Run the Python script with the specified mode, redirecting stdout and stderr
+python ../src/run_gvae_dense_rnn.py --mode $MODE > $OUTPUT_LOG 2> $ERROR_LOG
